073814253937e47ccf7e6487a9499d07ebfefe56
Lion'sCode <102724896+usamataleb@users.noreply.github.com>
2024-12-26 15:55:47 +0000

    Testimonial Added

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Testimonial: React.FC = () => {
-
   return (
-<div className="testimonial-section" style={{backgroundImage: "url(assets/images/img23.jpg);"}}>
-<div className="container">
-   <div className="row">
-      <div className="col-lg-10 offset-lg-1">
-         <div className="testimonial-inner testimonial-slider">
-            <div className="testimonial-item text-center">
-               <figure className="testimonial-img">
-                  <img src="src/assets/images/guider.jpeg" alt=""/>
-               </figure>
-               <div className="testimonial-content">
-                  <p>“The best things in life are the people we love, the places we’ve been, and all the memories we’ve made along the way.”</p>
+    <div
+      className="testimonial-section"
+      style={{ backgroundImage: "url(assets/images/avator.jpeg);" }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-10 offset-lg-1">
+            <div className="testimonial-inner testimonial-slider">
+              <div className="testimonial-item text-center">
+                <figure className="testimonial-img">
+                  <img src="src/assets/images/avator.jpeg" alt="" />
+                </figure>
+                <div className="testimonial-content">
+                  <p>
+                    “The best things in life are the people we love, the places
+                    we’ve been, and all the memories we’ve made along the way.”
+                  </p>
                   <cite>
-                     Salum Said
-                     <span className="company">Tour Operator</span>
+                    Salum Said
+                    <span className="company">Tour Operator</span>
                   </cite>
-               </div>
+                </div>
+              </div>
             </div>
-         </div>
+          </div>
+        </div>
       </div>
-   </div>
-</div>
-</div>
+    </div>
   );
 };
 
